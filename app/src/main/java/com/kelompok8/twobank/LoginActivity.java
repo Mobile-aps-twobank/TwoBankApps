fa1e92c2b8327f17c8c219126508ab683ee7da95
dewayp <dewayoganteng1@gmail.com>
2023-10-07 23:37:57 +0800

    komit FE login

--- app/src/main/java/com/kelompok8/twobank/LoginActivity.java
+++ app/src/main/java/com/kelompok8/twobank/LoginActivity.java
@@ -2,7 +2,10 @@ package com.kelompok8.twobank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Temukan tombol "Back" ImageView
+        ImageView backButton = findViewById(R.id.backButton);
+
+        // Menambahkan OnClickListener pada ImageView
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Kembali ke MainActivity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Untuk menutup LoginActivity
+            }
+        });
     }
 }
