c73f82d8b15700b8dd33f5bfb3c46849247c60b5
dewayp <dewayoganteng1@gmail.com>
2023-10-11 19:42:02 +0800

    komit NAVIGASI

--- app/src/main/java/com/kelompok8/twobank/EditProfileActivity.java
+++ app/src/main/java/com/kelompok8/twobank/EditProfileActivity.java
@@ -1,8 +1,13 @@
 package com.kelompok8.twobank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set the initial selected item
+        bottomNavigationView.setSelectedItemId(R.id.menu_item_1);
+
+        // Handle item clicks
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.menu_item_1) {
+                    Intent intent = new Intent(EditProfileActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_2) {
+                    Intent intent = new Intent(EditProfileActivity.this, SaldoActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_3) {
+                    Intent intent = new Intent(EditProfileActivity.this, ScanActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_4) {
+                    Intent intent = new Intent(EditProfileActivity.this, NotifikasiActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_5) {
+                }
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/kelompok8/twobank/ProfilActivity.java
+++ app/src/main/java/com/kelompok8/twobank/ProfilActivity.java
@@ -1,8 +1,14 @@
 package com.kelompok8.twobank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -10,5 +16,49 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set the initial selected item
+        bottomNavigationView.setSelectedItemId(R.id.menu_item_1);
+
+        // Handle item clicks
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.menu_item_1) {
+                    Intent intent = new Intent(ProfilActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_2) {
+                    Intent intent = new Intent(ProfilActivity.this, SaldoActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_3) {
+                    Intent intent = new Intent(ProfilActivity.this, ScanActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_4) {
+                    Intent intent = new Intent(ProfilActivity.this, NotifikasiActivity.class);
+                    startActivity(intent);
+                } else if (itemId == R.id.menu_item_5) {
+                }
+
+                return true;
+            }
+        });
+
+    }
+//        INI DARI PROFILE KE EDIT PROFILE
+    public void openEditProfile(View view) {
+        // Handle the click on the LinearLayout (open EditProfileActivity)
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
+    }
+
+//    INI LOGOUT YA
+    public void logout(View view) {
+        // Handle the click on the LinearLayout (perform logout action here)
+        // You can add your logout functionality here, e.g., clearing session, logging out the user, etc.
     }
 }
